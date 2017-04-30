971b7970acf0fb6bedaf6cf7d84facf4f3ef6529
michaelandrewellis <mikey1mae@gmail.com>
2017-04-30 13:21:08 +0100

    commented out "create database" instruction in dump

--- DB_dump_30:04:17.sql
+++ DB_dump_30:04:17.sql
@@ -1,5 +1,5 @@
---CREATE DATABASE  IF NOT EXISTS `biocomp_project` /*!40100 DEFAULT CHARACTER SET latin1 */;
---USE `biocomp_project`;
+-- CREATE DATABASE  IF NOT EXISTS `biocomp_project` /*!40100 DEFAULT CHARACTER SET latin1 */;
+-- USE `biocomp_project`;
 -- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
 -- Host: localhost    Database: biocomp_project
