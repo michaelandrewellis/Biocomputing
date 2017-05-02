77e31446d2450e9112f901f9350d8c867231b153
michaelandrewellis <mikey1mae@gmail.com>
2017-05-02 17:31:43 +0100

    Fixed dataframes in cgi-script

--- cgi-bin/cgi-script.py
+++ cgi-bin/cgi-script.py
@@ -84,15 +84,15 @@ if input_type == 'Chromosome_location':
     df = df[df['Arm']==arm]
     df = df[df['Start']<= position]
     df = df[df['End'] >= position]
-    html = df['Accession','Location','Protein Product'].to_html
+    html = df[['Accession','Location','Protein Product']].to_html
 elif input_type == 'Protein_product':
     df = pd.DataFrame.from_csv("../summarytable.csv")
     df = df[df['Protein Product']==input_value]
-    html = df['Accession','Location','Protein Product','Gene Name'].to_html
+    html = df[['Accession','Location','Protein Product','Gene Name']].to_html
 elif input_type == 'Gene_name':
     df = pd.DataFrame.from_csv("../summarytable.csv")
     df = df[df['Gene Name'] == input_value]
-    html = df['Accession', 'Location', 'Protein Product', 'Gene Name'].to_html
+    html = df[['Accession', 'Location', 'Protein Product', 'Gene Name']].to_html
 elif input_type == 'Gene_ID':
     [DNA, CDS_locs, codon_table, enzyme_table] = functions.get_data(input_value, 'Gene_ID')
     colours = ['Red', 'Yellow', 'Pink']
